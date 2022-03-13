fddddb7350d554eaf6fe2a17d00fcf7b56cad2a0
VladislavGurbatov <vlad.gurbatov@gmail.com>
2022-03-13 05:16:26 +0300

    lab 4 changed sort

--- laba_saod_4/laba_saod_4/Header.h
+++ laba_saod_4/laba_saod_4/Header.h
@@ -2,8 +2,6 @@
 namespace logic {
 	void fill_mass(int* x, int n);			// заполнение массивом вводом в консоль
 	int sort(int* x, int n);				// оптимальная сортировка
-	int sortfte(int* x, int n);				// сортировка от текущего элемента к концу массива
-	int sortetf(int* x, int n);				// сортировка от конца массива к текущему элементу
 	void text_out(int* x, int n, int c);	// вывод в текстовый файл
 	void fill_mass(int* x, int n, int* c);	// заполнение массива другим массивом
 	void swap(int& a, int& b);				// меняет местами знач. переменных
