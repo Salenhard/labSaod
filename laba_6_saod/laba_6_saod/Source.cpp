033a774d5eebb81d578004689d356d8825ada7dd
vlad gurbatov <vlad.gurbatov@gmail.com>
2022-04-08 13:03:20 +0900

    lab 6 update

--- laba_6_saod/laba_6_saod/Source.cpp
+++ laba_6_saod/laba_6_saod/Source.cpp
@@ -1,13 +1,3 @@
 #include "Header.h"
 
 
-void logic::fillRand(std::vector<int>& list, unsigned int n) {
-	list.resize(n);
-	for(unsigned int i = 0; i < n; i++)
-	list[i] = (rand() % (10 + i * 10)) + i * i * 20;
-}
-
-void logic::show(std::vector<int> list) {
-	for (int i = 0; i < list.size(); i++)
-		std::cout << "[" << i + 1 << "]: " << list[i] << " ";
-}
--- laba_6_saod/laba_6_saod/laba_6_saod.cpp
+++ laba_6_saod/laba_6_saod/laba_6_saod.cpp
@@ -2,72 +2,45 @@
 #include "Header.h"
 #include <ctime>
 
-template<class T>
-T binary_search(const std::vector<T>& list, const T& item)
-{
-	T low = 0;
-	T high = list.size() - 1;
-	while (low <= high)
-	{
-		T mid = (low + high);
-		T guess = list[mid];
-		if (guess == item)
-		{
-			return mid + 1;
-		}
-		else if (guess > item)
-		{
-			high = mid - 1;
-		}
-		else
-		{
-			low = mid + 1;
-		}
-	}
-	return NULL;
-}
-
-template<typename C>
-C cycle_search(const std::vector<C>& list, const C& item)
-{
-	for (int i = 0; i < list.size(); i++)
-		if (list[i] == item)
-			return i + 1;
-	return NULL;
-}
-
-
 using namespace std;
 
 int main()
 {
+	unsigned int start_time = clock(); // начальное время
+	
+	unsigned int end_time = clock(); // конечное время
+	unsigned int search_time = end_time - start_time; // искомое время
 	vector<int> list = { 1,2,3,4,5,6,7,8,9,10 };
 	vector<int> list2;
 	int n, c;
-	logic::show(list);
+	cout << logic::show(list);
 	cout << endl;
-	cout << "List binary search: " << binary_search(list, 5) << endl;
-	cout << "List cycle search: " << cycle_search(list, 5) << endl;
+	cout << "List binary search: " << logic::binary_search(list, 5) << endl;
+	cout << "List cycle search: " << logic::cycle_search(list, 5) << endl;
 
 	cout << "Vector size: ";
 	cin >> n;
 	logic::fillRand(list2, n);
-	logic::show(list2);
+	logic::selectionSort(list2);
+	cout << logic::show(list2) << endl;
 	cout << endl << "input number :";
 	cin >> c;
 	
 
-	double start_time = clock(); // начальное время
-	cout << "List 2 binary search: " << binary_search(list2, c) << endl;
-	double end_time = clock(); // конечное время
-	double search_time = end_time - start_time; // искомое время
-	cout << "Time: " << search_time / 1000.0 << endl;
-
-    start_time = clock(); // начальное время
-	cout << "List 2 cycle search: " << cycle_search(list2, c) << endl;
-    end_time = clock(); // конечное время
+	start_time = clock(); // начальное время
+	cout << "List 2 cycle search: " << logic::cycle_search(list2, c) << endl;
+	end_time = clock(); // конечное время
 	search_time = end_time - start_time; // искомое время
-	cout << "Time: " << search_time / 1000.0 << endl;
+	double time1 = search_time;
+	cout << "Time: " << time1 << " ms" << endl;
+
+	start_time = clock(); // начальное время
+	cout << "List 2 binary search: " << logic::binary_search(list2, c) << endl;
+	end_time = clock(); // конечное время
+	search_time = end_time - start_time; // искомое время
+	double time2 = search_time;
+	cout << "Time: " << time2 << " ms" << endl;
+	logic::fout(list2, time1, time2, c);
 	
 
 }
