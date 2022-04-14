e65687c03c1a898adef0b5c752514c4a74d4cdbf
VladislavGurbatov <vlad.gurbatov@gmail.com>
2022-04-15 00:37:03 +0300

    lab 9 is finished

--- Lab9/Lab9/Lab9.cpp
+++ Lab9/Lab9/Lab9.cpp
@@ -4,5 +4,23 @@
 using namespace std;
 int main()
 {
-	
+	Queue<int> digitQueue[10];
+	const int n = 75;
+	int L[n];
+
+	fillRand(L, n);
+	printArr(L, n);
+
+	distribute(L, digitQueue, n, ones);
+	collect(digitQueue, L);
+	printArr(L, n);
+
+	distribute(L, digitQueue, n, tens);
+	collect(digitQueue, L);
+	printArr(L, n);
+
+	distribute(L, digitQueue, n, hundreds);
+	collect(digitQueue, L);
+	printArr(L, n);
+
 }
--- Lab9/Lab9/Logic.h
+++ Lab9/Lab9/Logic.h
@@ -1,14 +1,45 @@
 #pragma once
-
+#include <iostream>
 #include "Queue.h"
-template<typename T>
-void sortq(T arr, int size) {
-	Queue q[10];
-	for (int i =0; i < 10;i++)
-	{
-		for (int k = 0; k < size; k++)
-		{
+enum DigitKind{ones, tens, hundreds};
+using namespace std;
 
+void distribute(int L[], Queue<int> digitQueue[], int n, DigitKind kind)
+{
+	for (int i = 0; i < n; i++)
+	{
+		if (kind == ones) 
+			digitQueue[L[i] % 10].insert(L[i]);
+		if (kind == tens)
+			digitQueue[(L[i] % 100) / 10].insert(L[i]);
+		if (kind == hundreds)
+			digitQueue[L[i] / 100].insert(L[i]);
+	}
+}
+
+void collect(Queue<int> digitQueue[], int L[]) {
+	int i = 0;
+	for (int digit = 0; digit < 10; digit++)
+	{
+		while (digitQueue[digit].empty() == false) {
+			L[i++] = digitQueue[digit].del();
 		}
+	}
+}
+
+void printArr(int L[], int n) {
+	int i = 0;
+	while (i < n) {
+		cout.width(5);
+		cout << L[i];
+		if (++i % 10 == 0)
+			cout << endl;
+	}
+	cout << endl;
+}
+
+void fillRand(int L[], int n) {
+	for (int i = 0; i < n; i++) {
+		L[i] = rand() % 1000;
 	}
 }
