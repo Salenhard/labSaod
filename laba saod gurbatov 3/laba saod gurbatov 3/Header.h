8b46803b4bdb7319364fd7cacb81909b784f0632
VladislavGurbatov <vlad.gurbatov@gmail.com>
2022-04-14 15:36:50 +0300

    deleted useless

--- laba saod gurbatov 3/laba saod gurbatov 3/Header.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-namespace logic {
-    unsigned int cycle(unsigned int a, unsigned int b);                   // цикл
-    unsigned int recurs(unsigned int a, unsigned int b);                   // рекурсия
-    void f_out(unsigned int a, unsigned int b);                                                // вывод в файл
-}
--- laba_5_saod/laba_5_saod/Header.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <String>
-#include <iostream>
-
-namespace dynamic {
-	void algoritm(int* x, int n);				// алгоритм динамич. программирования
-	void show(int* x, int n);					// вывод массиива в консоль
-}
