4027d229c7e5e238cf8e564c3836280ae3f7456f
VladislavGurbatov <vlad.gurbatov@gmail.com>
2022-04-14 15:27:19 +0300

    added lab 9

---
+++ Lab9/Lab9/Lab9.cpp
@@ -0,0 +1,8 @@
+﻿#include <iostream>
+#include "Queue.h"
+#include "Logic.h"
+using namespace std;
+int main()
+{
+	
+}
---
+++ Lab9/Lab9/Logic.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "Queue.h"
+template<typename T>
+void sortq(T arr, int size) {
+	Queue q[10];
+	for (int i =0; i < 10;i++)
+	{
+		for (int k = 0; k < size; k++)
+		{
+
+		}
+	}
+}
