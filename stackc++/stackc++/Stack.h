5049638014777ebfe6f69864fd1d0e48505872bb
Salenhard <77225146+Salenhard@users.noreply.github.com>
2022-04-29 13:27:05 +0900

    Update Stack.h

--- stackc++/stackc++/Stack.h
+++ stackc++/stackc++/Stack.h
@@ -1,17 +1,18 @@
 #include <string>
 #include<iostream>
 #pragma once
+const int MAXSIZE = 50;
 template<typename T> class Stack {
     T* pStack;
     int top;
 public:
     Stack() {
-        this->pStack = new T [50];
+        this->pStack = new T [MAXSIZE];
         this->top = 0;
     }
     void push(const T &item)
     {
-        if (top + 1 != 50) {
+        if (top + 1 != MAXSIZE) {
             this->pStack[this->top] = item;
             this->top++;
         }
@@ -28,7 +29,7 @@ public:
         else throw std::invalid_argument("Stack is empty!");
     }
     void clearStack(){
-        std::memset(&(pStack[0]), 0, 50);
+        std::memset(&(pStack[0]), 0, MAXSIZE);
         top = 0;
     }
     T peek() {
